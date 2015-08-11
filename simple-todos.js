87c3c6e082a9b9bb1ffa2d3a97d76376dae2bc1c
Joshua Mun <joshmun23@gmail.com>
2015-08-10 20:44:32 -0400

    add form and template event listener on body, capture target text to store into db in js file

--- simple-todos.js
+++ simple-todos.js
@@ -7,6 +7,23 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   })
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      //  Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      event.target.text.value = '';
+    }
+  });
 }
 
 if (Meteor.isServer) {
